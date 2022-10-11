3513a83e80d65af10eb5d6d004ab008b0259e0ea
Aren Babikian <aren.babikian@mail.mcgill.ca>
2022-10-10 22:01:39 -0400

    dd feature exraction code from pretrained model

---
+++ correlation/other/get_features_from_pretrained.py
@@ -0,0 +1,64 @@
+import yaml
+from correlation.gnn.config import SceneReconstructionConfig
+from correlation.gnn.dataset import SceneGraphDatasetModule
+from correlation.gnn.models.gnn_model import SceneConstructionModule
+import torch
+from tqdm import tqdm
+import numpy as np
+import h5py
+
+
+def main(opt):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    for model_name, model_path in opt.model_paths.items():
+        print(model_name)
+        print(model_path)
+        print()
+
+        # Load the model
+        model = SceneConstructionModule.load_from_checkpoint(model_path, args=opt)
+        model.to(device)
+
+        # Define input graph structure
+        # These are defined in _datasets/IEPVQA/processed/data-{something}
+        # note that these also contain the gt output y (either random, or the features derived from the valudation images (resnet), saved at _data/iepvqa/val_features_0_3000.h5)
+
+        opt.intermediate_gt = model_name
+        dataloader = SceneGraphDatasetModule(opt).train_dataloader()
+
+        dataset_path = f'_data/{model_name}_features_{opt.min_id}_{opt.max_id}.h5'
+        with h5py.File(dataset_path, 'w') as f:
+
+            all_features_dataset = f.create_dataset('features', (opt.max_id-opt.min_id, 1024, 14, 14),
+                                        dtype=np.float32)
+
+            for batch_graph, num_obj_seq, image_indices in tqdm(dataloader):
+                # print(batch_graph)
+                # print(image_indices)
+
+                features_per_node = model.forward(batch_graph)
+
+                # derive feature per model from feature per node
+                features_per_model = [] 
+                start_id = 0
+                for n in num_obj_seq:
+                    features_per_model.append(torch.mean(features_per_node[start_id:start_id+n.data], 0).reshape(1024, 14, 14))
+                    start_id = start_id+n.data
+                
+                assert image_indices.size(dim=0) == len(features_per_model)
+                # ADD to dataset
+                for i, image_i in enumerate(image_indices):
+                    all_features_dataset[image_i] = features_per_model[i].detach().numpy()
+
+                # print(len(features_per_model))
+                # print(features_per_model[0].size())
+
+
+if __name__ == "__main__":
+
+    with open("correlation/other/feature_extraction_config.yaml") as fp:
+        dataMap = yaml.safe_load(fp)
+
+    config = SceneReconstructionConfig(dataMap)
+    main(config)
